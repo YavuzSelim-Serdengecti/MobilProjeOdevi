f43a85a882fccf0d8c3c42d4e264b8562aa1a6a7
Yavuz Selim Serdengeçti <161735380+YavuzSelim-Serdengecti@users.noreply.github.com>
2025-05-21 22:45:17 +0300

    Anasayfadaki liste mantığı oluşturuldu

--- screens/CountryListScreen.js
+++ screens/CountryListScreen.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 
-export default function CountryListScreen() {
+const mockCountries = [
+  {
+    name: 'Türkiye',
+    flag: 'https://flagcdn.com/w320/tr.png',
+  },
+  {
+    name: 'Almanya',
+    flag: 'https://flagcdn.com/w320/de.png',
+  },
+  {
+    name: 'Fransa',
+    flag: 'https://flagcdn.com/w320/fr.png',
+  },
+];
+
+const CountryListScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Ana Sayfa - Ülke Listesi</Text>
+      <FlatList
+        data={mockCountries}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={{ uri: item.flag }} style={styles.flag} />
+            <Text style={styles.countryName}>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  container: { flex: 1, padding: 16, marginTop:30, backgroundColor: '#fff' },
+  card: {
+    flexDirection: 'row',
     alignItems: 'center',
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 10,
+    backgroundColor: '#f0f0f0',
+  },
+  flag: {
+    width: 60,
+    height: 40,
+    borderRadius: 4,
+    marginRight: 16,
+  },
+  countryName: {
+    fontSize: 18,
+    fontWeight: '600',
   },
 });
+
+export default CountryListScreen;
